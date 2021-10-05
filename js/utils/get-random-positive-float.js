3ff853b901440e2d859b65cccfce0c4dfdcb27a7
Keks <keks@htmlacademy.ru>
2021-10-05 20:57:45 +0300

    :package:

--- js/utils/get-random-positive-float.js
+++ js/utils/get-random-positive-float.js
@@ -20,4 +20,3 @@ function getRandomPositiveFloat (a, b, digits = 1) {
   // указать требуемое количество знаков после точки
   return result.toFixed(digits);
 }
-getRandomPositiveFloat(1.56, 1.7, 3);
