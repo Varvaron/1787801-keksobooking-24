eb689ef0be52333198671dc81fa243c9a7d4c528
Варвара <varya.kuch@gmail.com>
2021-10-24 12:26:23 +0500

    добавление иконки для главной метки

--- js/map.js
+++ js/map.js
@@ -4,10 +4,10 @@ switchInactiveMode();
 const map = L.map('map-canvas').on('load', () => {
   switchActiveMode();
 })
-.setView({
-  lat: 35.681729,
-  lng: 139.753927,
-}, 10);
+  .setView({
+    lat: 35.681729,
+    lng: 139.753927,
+  }, 10);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -15,3 +15,21 @@ L.tileLayer(
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '/img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPin = L.marker(
+  {
+    lat: 35.681729,
+    lng: 139.753927,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+mainPin.addTo(map);
