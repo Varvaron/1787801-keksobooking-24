328c04e200f6c3a171fc0105c66ae198b3b41ae0
Варвара <varya.kuch@gmail.com>
2021-10-30 18:50:08 +0500

    добавила проверку на ошибки

--- js/api.js
+++ js/api.js
@@ -1,6 +1,12 @@
 import {createMarker} from './map.js';
 
 fetch('https://24.javascript.pages.academy/keksobooking/data')
+  .then((response) => {
+    if (response.ok) {
+      return response;
+    }
+    throw new Error(`${response.status} — ${response.statusText}`);
+  })
   .then((response) => response.json())
   .then((similarOffers) => {
     createMarker(similarOffers.slice(0, 10));
