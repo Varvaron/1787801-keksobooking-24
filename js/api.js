d8f718c4cefaf136fdd3abf067904d2ee41eb2a5
Варвара <varya.kuch@gmail.com>
2021-10-30 17:59:39 +0500

    оставляет только 10 меток на карте

--- js/api.js
+++ js/api.js
@@ -1,8 +1,7 @@
-import {renderPopup} from './similar-offers.js';
 import {createMarker} from './map.js';
 
 fetch('https://24.javascript.pages.academy/keksobooking/data')
   .then((response) => response.json())
   .then((similarOffers) => {
-    createMarker(similarOffers);
+    createMarker(similarOffers.slice(0, 10));
   });
