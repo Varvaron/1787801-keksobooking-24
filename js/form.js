83ab506a48dcc2be1883a7e1be1a960ff2bf16ab
Варвара <varya.kuch@gmail.com>
2021-10-31 11:24:09 +0500

    отправка формы по ajax

--- js/form.js
+++ js/form.js
@@ -32,12 +32,12 @@ titleInput.addEventListener('input', () => {
   const titleLength = titleInput.value.length;
   if (titleLength < MIN_TITLE_LENGTH) {
     titleInput.setCustomValidity(`Мин. длина заголовка - ${ MIN_TITLE_LENGTH} симв. Введите еще ${ MIN_TITLE_LENGTH - titleLength } симв.`);
-    titleInput.style = 'box-shadow: 0 0 2px 2px red';
+    titleInput.style.boxShadow = '0 0 2px 2px red';
   } else if (titleLength > MAX_TITLE_LENGTH) {
     titleInput.setCustomValidity(`Макс. длина заголовка - ${ MAX_TITLE_LENGTH} симв. Удалите еще ${ titleLength - MAX_TITLE_LENGTH } симв.`);
   } else {
     titleInput.setCustomValidity('');
-    titleInput.style = '';
+    titleInput.style.boxShadow = '';
   }
   titleInput.reportValidity();
 });
@@ -46,10 +46,10 @@ priceInput.addEventListener('input', () => {
   const priceValue = priceInput.value;
   if (priceValue > MAX_PRICE_VALUE) {
     priceInput.setCustomValidity(`Макс. цена за ночь - ${ MAX_PRICE_VALUE}`);
-    priceInput.style = 'box-shadow: 0 0 2px 2px red';
+    priceInput.style.boxShadow = '0 0 2px 2px red';
   } else {
     priceInput.setCustomValidity('');
-    priceInput.style = '';
+    priceInput.style.boxShadow = '';
   }
   priceInput.reportValidity();
 });
@@ -58,7 +58,7 @@ priceInput.addEventListener('input', () => {
 const onRoomCapacityChange = () => {
   const rooms = +roomNumberSelect.value;
   const capacity = +capacitySelect.value;
-  capacitySelect.style = 'box-shadow: 0 0 2px 2px red';
+  capacitySelect.style.boxShadow = '0 0 2px 2px red';
   if (rooms < capacity) {
     capacitySelect.setCustomValidity('Кол-во гостей превышает кол-во комнат');
   } else if (rooms === 100 && capacity !== 0) {
@@ -67,7 +67,7 @@ const onRoomCapacityChange = () => {
     capacitySelect.setCustomValidity('Вариант "Не для гостей" доступен только для 100 комнат');
   } else {
     capacitySelect.setCustomValidity('');
-    capacitySelect.style = '';
+    capacitySelect.style.boxShadow = '';
   }
   capacitySelect.reportValidity();
 };
@@ -75,11 +75,23 @@ const onRoomCapacityChange = () => {
 roomNumberSelect.addEventListener('change', onRoomCapacityChange);
 capacitySelect.addEventListener('change', onRoomCapacityChange);
 
-form.addEventListener('submit', (evt) => {
-  if (!capacitySelect.checkValidity()) {
-    capacitySelect.style = 'box-shadow: 0 0 2px 2px red';
+const setUserFormSubmit = (onSuccess) => {
+  form.addEventListener('submit', (evt) => {
+    if (!capacitySelect.checkValidity()) {
+      capacitySelect.style.boxShadow = '0 0 2px 2px red';
+      evt.preventDefault();
+    }
     evt.preventDefault();
-  }
-});
+    const formData = new FormData(evt.target);
+    fetch(
+      'https://24.javascript.pages.academy/keksobooking',
+      {
+        method: 'POST',
+        body: formData,
+      },
+    );
+  })
+}
 
-export {switchInactiveMode, switchActiveMode};
+
+export {switchInactiveMode, switchActiveMode, setUserFormSubmit};
